f0d78846c007e8562f69f3a57fd68c7c8fd0b34c
dnl <info@dnl.re>
2017-05-18 19:41:34 +0200

    formatting

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
-$(function(){
+$(function () {
 
     var shell = require('shell');
     var fs = require('fs');
 
     function extractChordsObjects(song) {
-        
-        function buildChordObject(lineArray){
+
+        function buildChordObject(lineArray) {
             return { "charPosition": lineArray[0], "lineNumber": lineArray[1], "chord": lineArray[2] };
         }
 
         function writeChordsIntoArray(songBeamerChords) {
             var linesArray = [];
             var lines = songBeamerChords.split('\r');
-            lines.forEach(function(line){
+            lines.forEach(function (line) {
                 linesArray.push(buildChordObject(line.split(',')));
             });
             return linesArray;
         }
 
-        function extractSongBeamerChords (song) {
+        function extractSongBeamerChords(song) {
             var guitarChords = "" + /(#Chord).+/g.exec(song);
-            guitarChords = guitarChords.replace(/(#Chords=)/g,'');
-            guitarChords = guitarChords.replace(/(,#Chord)/g,'');
+            guitarChords = guitarChords.replace(/(#Chords=)/g, '');
+            guitarChords = guitarChords.replace(/(,#Chord)/g, '');
             return new Buffer(guitarChords, 'base64').toString('utf8');
         }
 
@@ -30,15 +30,15 @@ $(function(){
         return arrayOfChordObjects;
     };
 
-    fs.readFile(__dirname + '/input.sng', 'binary', function(err, contents){
+    fs.readFile(__dirname + '/input.sng', 'binary', function (err, contents) {
         if (err) { return console.log(err); }
 
         var totalOutput = contents;
         var chords = "";
-        extractChordsObjects(contents).forEach(function (el){ // only for output
+        extractChordsObjects(contents).forEach(function (el) { // only for output
             chords += JSON.stringify(el) + '<br>';
         });
-        
+
         $('#partial-output').html(chords);
         $('#total-output').html(totalOutput.replace(/(?:\r\n|\r|\n)/g, '<br />'));
     });
