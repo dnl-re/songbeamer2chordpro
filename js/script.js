1fd16927dc958f9de37c8a3738902d938a3dfa12
dnl <info@dnl.re>
2017-05-18 17:18:37 +0200

    extracted chordObjects from song

--- js/script.js
+++ js/script.js
@@ -18,16 +18,37 @@ $(function(){
 
 
     function extractChords(song) {
+
+        function writeChordsIntoArray(rawChords) {
+            var linesArray = [];
+            var lines = rawChords.split('\r');
+            lines.forEach(function(line){
+                var lineArray = line.split(',');
+                var chordObject = {
+                    "charPosition": lineArray[0],
+                    "lineNumber": lineArray[1],
+                    "chord": lineArray[2]
+                };
+                linesArray.push(chordObject)
+            });
+            return linesArray;
+        }
+
         var guitarChords = "" + /(#Chord).+/g.exec(song);
         guitarChords = guitarChords.replace(/(#Chords=)/g,'');
         guitarChords = guitarChords.replace(/(,#Chord)/g,'');
-        return new Buffer(guitarChords, 'base64').toString('utf8');
+        rawChords = new Buffer(guitarChords, 'base64').toString('utf8');
+        var arrayOfChordObjects = writeChordsIntoArray(rawChords);
+        return arrayOfChordObjects;
     };
 
     fs.readFile(__dirname + '/input.sng', 'binary', function(err, contents){
         if (err) { return console.log(err); }
         var totalOutput = contents;
-        var chords = extractChords(contents);
+        var chords = "";
+        extractChords(contents).forEach(function (el){
+            chords += JSON.stringify(el) + '<br>';
+        });
         
         
         $('#partial-output').html(chords);
