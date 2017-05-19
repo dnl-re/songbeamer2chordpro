9b2bb5c3d1de0dcea4e7ee493cb893202d12d43b
dnl <info@dnl.re>
2017-05-19 18:07:19 +0200

    returns song chunks

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(function () {
     var fs = require('fs');
 
     var chordObjects = [];
+    var songPartObjects = [{ a: "1" }, { b: "2" }];
 
     function extractChordObjects(song) {
 
@@ -32,23 +33,40 @@ $(function () {
         return arrayOfChordObjects;
     };
 
+    function extractSongPartObjects(song) {
+        song = song.replace(/([^-]--\r\n)|([^-]---\r\n)/g, '---');
+        song = song.split('---');
+        return songPartObjects = song.splice(1);
+    }
+
     function displayData(fileData) {
 
-        function displayChordObjects(chordObjects) {
+        function displayChordObjects() {
             var chords = "";
-            chordObjects.forEach(function (el) { // only for output
+            chordObjects.forEach(function (el) {
                 chords += JSON.stringify(el) + '<br>';
             });
             $('#partial-output').html(chords);
         }
 
-        displayChordObjects(chordObjects)
+        function displaySongPartObjects() {
+            var chords = "";
+            songPartObjects.forEach(function (el) {
+                chords += JSON.stringify(el) + '<br>';
+            });
+            $('#partial-output').html(chords);
+            console.log(songPartObjects);
+        }
+
+        // displayChordObjects(chordObjects);
+        displaySongPartObjects(songPartObjects);
         $('#total-output').html(fileData.replace(/(?:\r\n|\r|\n)/g, '<br />'));
     }
 
     fs.readFile(__dirname + '/input.sng', 'binary', function (err, fileData) {
         if (err) { return console.log(err); }
         chordObjects = extractChordObjects(fileData);
-        displayData(fileData)
+        songPartObjects = extractSongPartObjects(fileData);
+        displayData(fileData);
     });
 });
