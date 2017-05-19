5c10c59cbcc5d16d4b320b832d5a8c568868c4b7
dnl <info@dnl.re>
2017-05-19 22:11:42 +0200

    now returning songPartObject

--- js/script.js
+++ js/script.js
@@ -54,20 +54,18 @@ $(function () {
     function extractSongPartObjects(song) {
 
         function buildSongPartObect(part) {
+            var songPartObject = {}
             var numberOfLanguages = parseInt(metaData.LangCount);
             var languages = [];
-            
             var lines = part.match(/.*\r/g);
             for (i = 0; i < numberOfLanguages; i++) {
                 var singleLanguageLines = [];
-                for (j = i; j < lines.length; j+= numberOfLanguages) {
+                for (j = i; j < lines.length; j += numberOfLanguages) {
                     singleLanguageLines.push(lines[j]);
                 }
-                languages[i] = singleLanguageLines;
+                songPartObject["language" + (i + 1)] = singleLanguageLines.toString();
             }
-            console.log(languages);
-            
-            return { part: part };
+            return songPartObject;
         }
 
         function buildSongPartsObect(song) {
@@ -103,7 +101,7 @@ $(function () {
     function measurePerformanceOfFunction(functionToMeasure, functionParameter) {
         var loop = 1000;
         var sum = 0;
-        for (var i=0; i <= loop; i++) {
+        for (var i = 0; i <= loop; i++) {
             var t0 = performance.now();
             functionToMeasure(functionParameter);
             var t1 = performance.now();
